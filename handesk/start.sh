873d31b186199eb9db2ead81e4c516cca084a33c
inder4u <getinder4u@gmail.com>
2022-12-11 20:06:55 +0530

    Update start.sh

--- handesk/start.sh
+++ handesk/start.sh
@@ -1,7 +1,5 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
-echo 'Add database info in .env file and enter Y'
-read -p "Y" choice
 if [ ! -d "/home/container/webroot/handesk/vendor" ]; then echo 'Add database info in .env file and enter Y' && read -p "Y" choice && cd /home/container/webroot/handesk && composer install --no-dev --optimize-autoloader && php artisan key:generate --force && php artisan config:cache && php artisan config:clear && php artisan migrate --seed && php artisan db --seed; fi
 
 echo "⟳ Starting PHP-FPM..."
