3d87b9e97f8faea6f0ab855e700df0630b1479a3
inder4u <getinder4u@gmail.com>
2023-01-02 11:17:19 +0530

    Update start.sh

--- phpservermon/start.sh
+++ phpservermon/start.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-if [ ! -d "/home/container/webroot/sm/vendor" ]; then cd /home/container/webroot/sm && composer install --no-dev --optimize-autoloader; fi
+if [ ! -d "/home/container/webroot/sm/vendor" ]; then cd /home/container/webroot/sm && composer install --no-dev --optimize-autoloader echo 'Add database info in config.php file and enter Y' && read -p "Y" choice; fi
 echo "⟳ Starting PHP-FPM..."
 /usr/sbin/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
